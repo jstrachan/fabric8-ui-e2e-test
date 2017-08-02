9a97781eec63b7a23c2e49b516c87a4a1a135c5f
James Strachan <james.strachan@gmail.com>
2017-08-02 09:33:29 +0100

    Update release.groovy

--- release.groovy
+++ release.groovy
@@ -1,6 +1,5 @@
 #!/usr/bin/groovy
 def ci (){
-    commenting out functional tests until we look at this error in the pipeline: `geckodriver: setting permissions to 0755 for /home/jenkins/workspace/-projects_fabric8-ui_PR-668-`
     stage('functional test'){
        sh './run_functional_tests.sh'
     }
